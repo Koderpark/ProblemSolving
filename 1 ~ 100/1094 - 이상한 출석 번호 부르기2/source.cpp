81b537d587e4bc41ba1bc0073392a5c6851f3e79
Koder0205 <Koder0205@users.noreply.github.com>
2018-11-06 18:35:35 +0900

    11-06 update

---
+++ 1 ~ 100/1094 - 이상한 출석 번호 부르기2/source.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	char array[n];
+	for(int i=0 ; i<n ; i++){
+		scanf("%d", &array[i]);
+	}
+	for(int i=n - 1 ; i>=0 ; i--){
+		printf("%d ", array[i]);
+	}
+}
