e7308aa78a1c49b11c228ae41975797e01970723
Koder0205 <Koder0205@users.noreply.github.com>
2018-11-24 17:57:59 +0900

    update

---
+++ 1401 ~ 1500/1477 - 2차원 배열 빗금 채우기 3-2/source.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+  
+int main(){
+    int data[101][101] = {0};
+    int n,m;
+    int a = 0;
+    scanf("%d %d", &n, &m);
+      
+    for(int s= 0; s<= n+m - 1; s++){
+        for(int i=0; i<n; i++){
+            int j=s-i;
+            if(j >= 0 && j < m){
+                a++;
+                data[i][j] = a;
+            }
+        }
+    }
+      
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            printf("%d ", data[i][j]);
+        }
+        printf("\n");
+    }
+  	return 0;
+}
