85c298fd29ac374ab4a479a6b0085cfd69e715b3
Koder0205 <Koder0205@users.noreply.github.com>
2019-01-14 12:16:51 +0900

    update

---
+++ 14XX/1403 - 배열 두번 출력하기/source.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int k,arr[101] = {0};
+	scanf("%d", &k);
+	for(int i=0; i<k; i++) scanf("%d", &arr[i]);
+	for(int i=0; i<2*k; i++){
+		printf("%d", arr[i%k]);
+	}
+	return 0;
+}
