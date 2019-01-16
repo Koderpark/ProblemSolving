458bd5db1c8c9bc027393ef0d5dbea402f15178f
Koder0205 <Koder0205@users.noreply.github.com>
2019-01-16 11:46:37 +0900

    update

--- 14XX/1451 - 데이터 정렬 (small)/source.cpp
+++ 14XX/1451 - 데이터 정렬 (small)/source.cpp
@@ -1,24 +1,12 @@
 #include <stdio.h>
- 
+#include <algorithm>
+
 int main(){
-    int n;
-    int a;
-    int data[10000];
-    scanf("%d", &n);
- 
-    for(int i=0; i<n; i++){
-        scanf("%d", &data[i]);
-    }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            if(data[i] < data[j]){
-                a = data[i];
-                data[i] = data[j];
-                data[j] = a;
-            }
-        }
-    }
-    for(int i=0; i<n; i++){
-        printf("%d\n", data[i]);
-    }
+	int n;
+	int arr[10001] = {0};
+	scanf("%d", &n);
+	for(int i=0; i<n; i++) scanf("%d", &arr[i]);
+	std::sort(arr, arr+n);
+	for(int i=0; i<n; i++) printf("%d\n", arr[i]);
+	return 0;
 }
