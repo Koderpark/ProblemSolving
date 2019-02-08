1909ed009b1059636c0eae2f1a71f944ad7d166d
Koder0205 <Koder0205@users.noreply.github.com>
2019-02-08 20:14:36 +0900

    update
    1488번 완성 안됨
    다시 풀어볼것
    반복횟수가 더 많은 문제

---
+++ 14XX/1488 - 2차원 배열 달팽이 채우기 4-5/source.cpp
@@ -0,0 +1,36 @@
+ 
+#include <stdio.h>
+int main()
+{
+   	int n=0, i=0, j=0, s=1;
+   	int arr[101][101]={0};
+ 
+   	int a=0, b=0;
+   	
+   	scanf("%d",&a);
+   	scanf("%d",&b);
+   	
+	int e=a, r=b;
+	
+	j=b+1;
+	i=a;
+	while(b>1){
+		s*=-1;
+      	for(int p=1;p<=b;p++){
+         	j+=s;
+        	arr[i][j]=++n;
+      	}
+      	if(--a>0){
+      		for(int p=1;p<=a;p++){
+           		i+=s;
+            	arr[i][j]=++n;
+        	}
+      	}
+      	b-=1;
+	}
+	
+	for(int i=1; i<=e; i++){
+        for(int j=1; j<=r; j++)printf("%d ",arr[i][j]);
+        printf("\n");
+    }
+}
