5b62f8cdfb330cc0e3b044e45de0cb28a79c3e46
Koder <43494256+Koder0205@users.noreply.github.com>
2019-05-15 19:56:11 +0900

    문자열

---
+++ 16XX/1650 - CODEUP/source.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+char text[8][36] = {
+	" ****  ***  ***   ***** *   * ****",
+	"*     *   * *  *  *     *   * *   *",
+	"*     *   * *   * *     *   * *   *",
+	"*     *   * *   * ****  *   * ****",
+	"*     *   * *   * *     *   * *",
+	"*     *   * *   * *     *   * *",
+	" ****  ***  ***   *****  ***  *"
+};
+
+int main(){
+	int w,h;
+	scanf("%d %d", &w, &h);
+	for(int x=0; x<7; x++){
+		for(int i=0; i<h; i++){
+			for(int y=0; y<strlen(text[x]); y++){
+				for(int j=0; j<w; j++){
+					printf("%c", text[x][y]);
+				}
+			}
+			if(!(x == 6 && i == h-1)) puts("a");
+		}
+	}
+	return 0;
+}
