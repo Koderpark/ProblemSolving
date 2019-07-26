3f21d1d30aff14ac7b9f04cec05656998c7ddc76
Koder <43494256+Koder0205@users.noreply.github.com>
2019-07-26 11:34:51 +0900

    update

--- 17XX/1722 - 여러 점 간의 거리/source.cpp
+++ 17XX/1722 - 여러 점 간의 거리/source.cpp
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
-int arr[2][101] = {0};
+#include <math.h>
 
 int main(){
 	int n;
-	float sum=0;
+	float x[101], y[101], sum=0;
 	scanf("%d", &n);
-	scanf("%d %d", &arr[0][0], &arr[1][0]);
-	for(int i=1; i<=n; i++){
-		scanf("%d %d", &arr[0][i], &arr[1][i]);
-		sum+=((arr[0][i] - arr[0][i-1]) * (arr[0][i] - arr[0][i-1]) + (arr[1][i] - arr[1][i-1]) * (arr[1][i] - arr[1][i-1]))/((arr[0][i] - arr[0][i-1]) * (arr[0][i] - arr[0][i-1]) + (arr[1][i] - arr[1][i-1]) * (arr[1][i] - arr[1][i-1]));
-		printf("%f\n", sum);
-	}
+	for(int i=0; i<n; i++) 	  scanf("%f %f", &x[i], &y[i]);
+	for(int i=0; i<n-1; i++)  sum+=sqrt((x[i]-x[i+1])*(x[i]-x[i+1])+(y[i]-y[i+1])*(y[i]-y[i+1]));
+	
+	printf("%.2f", sum);
+	
 	return 0;
 }
