e83a9edf4d3212099d5b0469f15af616993efe58
박성훈 / Koder <Koder0205@users.noreply.github.com>
2020-01-07 23:35:19 +0900

    ####중요####
    산술 오버플로우 땜에 틀린거같음
    다음에 문제풀때는 산술오버플로우도 고려합시다.

--- BOJ/1712 - 손익분기점/source.cpp
+++ BOJ/1712 - 손익분기점/source.cpp
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 int main(){
-	int a,b,c,i=-1;
-	scanf("%d %d %d", &a, &b, &c);
+	long long int a,b,c,i=-1;
+	scanf("%lld %lld %lld", &a, &b, &c);
 	if(c<=b){
 		printf("-1");
 		return 0;
 	}
-	while(a+(b*++i) >= c*i);
-	printf("%d", i);
+	printf("%lld", a/(c-b)+1);
 	return 0;
 }
