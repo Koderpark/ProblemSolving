39d0d050edf9a86c7f62fdee525fe5d992de22c5
박성훈 / Koder <Koder0205@users.noreply.github.com>
2020-01-15 23:54:13 +0900

    codeforces
    쬐끔 풀어본것

---
+++ CodeForces/B. Hyperset/source.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	char arr[1501][31] = {0};
+	int n,k;
+	scanf("%d %d", &n, &k);
+	for(int i=0; i<n; i++)	scanf("%s", &arr[i]);
+	for(int i=0; i<n; i++){
+		for(int j=0; j<k; j++){
+			sc=ec=tc=0;
+			
+		}
+	}
+	
+}
