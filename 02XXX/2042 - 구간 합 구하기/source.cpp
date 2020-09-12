91ef6759e3727aaa40e86a962105e3b7fcf5f5a8
박성훈 / Koder <koder0205@gmail.com>
2020-09-12 23:20:48 +0900

    실버V 문제풀이

--- 02XXX/2042 - 구간 합 구하기/source.cpp
+++ 02XXX/2042 - 구간 합 구하기/source.cpp
@@ -1,48 +1,48 @@
-#include <stdio.h>
-#include <vector>
+#include<stdio.h>
 
-using namespace std;
 typedef long long int ll;
+ll tree[3000000];
+ll num[1000001];
 
-vector<ll> tree(4000001);
-vector<ll> arr(1000001);
-
-ll init(int node, int start, int end){
-    if (start == end) return tree[node] = arr[start];
-    int mid = (start + end) / 2;
-    return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
+void update(int n, int s, int e, int t, int diff){
+    if (s<=t && t<=e) tree[n] += diff;
+    else return;
+    if (s == e) return;
+    
+    int m = (s + e) / 2;
+    update(n * 2, s, m, t, diff);
+    update(n * 2 + 1, m + 1, e, t, diff);
 }
 
-void update(int node, int start, int end, int index, ll diff)
-{
-    if (!(start <= index && index <= end)) return;
-    tree[node] += diff;
-    if(start != end)
-    {
-        int mid = (start + end) / 2;
-        update(node * 2, start, mid, index, diff);
-        update(node * 2 + 1, mid + 1, end, index, diff);
-    }
+ll sum(int l, int r, int n, int s, int e){
+    if (l <= s && e <= r) return tree[n];
+    if (r < s || e < l)   return 0;
+ 
+    int m = (s + e) / 2;
+    return sum(l, r, n * 2, s, m) + sum(l, r, n * 2 + 1, m + 1, e);
 }
 
-ll sum(int node, int start, int end, int left, int right){
-    if (left > end || right < start) return 0;
-    if (left <= start && end <= right) return tree[node];
-    int mid = (start + end) / 2;
-    return sum(node * 2, start, mid, left, right) + sum(node*2+1, mid+1, end, left, right);
+ll init(int n, int s, int e){
+    if (s == e) return tree[n] = num[s];
+    
+    int m = (s + e) / 2;
+    tree[n] = init(n * 2, s, m) + init(n * 2 + 1, m + 1, e);
+    return tree[n];
 }
 
 int main(){
-	int n,m,k;
-	ll  a,b,c;
-	scanf("%d %d %d", &n, &m, &k);
-	for(int i=1; i<=n;   i++) scanf("%lld", &arr[i]);
-	init(1, 0, n-1);
-	for(int i=1; i<=m+k; i++){
-		scanf("%lld %lld %lld", &a, &b, &c);
-		if(a==1) update(1,0,n-1,b,c);
-		if(a==2) printf("%lld\n", sum(1,0,n-1,b-1,c-1));
-	}
-	return 0;
+    int n,m,k;
+    int a,b,c;
+    int diff;
+    scanf("%d %d %d", &n, &m, &k);
+    for(int i=1; i<=n; i++) scanf("%lld", &num[i]);
+    
+    init(1, 1, n);
+    
+    for(int i=0; i<m+k; i++){
+        scanf("%d %d %d", &a, &b, &c);
+        if(a==1){ diff = c-num[b]; num[b]=c; update(1, 1, n, b, diff); }
+        else	  printf("%lld\n", sum(b, c, 1, 1, n));
+    }
+    return 0;
 }
-
