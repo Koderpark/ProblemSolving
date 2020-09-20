78a39864dd9bc18fba20fe3eaaa2bfbc88c0218c
박성훈 / Koder <koder0205@gmail.com>
2020-09-20 22:53:49 +0900

    update
    아 ㅋㅋ

--- 18XXX/18186 - 라면 사기 (Large)/source.cpp
+++ 18XXX/18186 - 라면 사기 (Large)/source.cpp
@@ -1,35 +1,37 @@
 #include <stdio.h>
 
-int ramen[1000010] = {0};
-long long int cost=0;
-int n,b,c;
+typedef long long int ll;
 
-int min(int a, int b){return a>b?b:a;}
+ll ramen[1000010] = {0};
+ll cost=0;
+ll n,b,c;
 
-void three(int k){
-	int t = min(ramen[k],min(ramen[k+1], ramen[k+2]));
+ll min(ll a, ll b){return a>b?b:a;}
+
+void three(ll k){
+	ll t = min(ramen[k],min(ramen[k+1], ramen[k+2]));
 	ramen[k]-=t;
 	ramen[k+1]-=t;
 	ramen[k+2]-=t;
 	cost+=(b+2*c)*t;
 }
 
-void two(int k){
-	int t = min(ramen[k],ramen[k+1]);
+void two(ll k){
+	ll t = min(ramen[k],ramen[k+1]);
 	ramen[k]-=t;
 	ramen[k+1]-=t;
 	cost+=(b+c)*t;
 }
 
-void one(int k){ cost+=b*ramen[k]; }
+void one(ll k){ cost+=b*ramen[k]; }
 
 
 int main(){
-	scanf("%d %d %d", &n, &b, &c);
-	for(int i=0; i<n; i++) scanf("%d", &ramen[i]);
+	scanf("%lld %lld %lld", &n, &b, &c);
+	for(int i=0; i<n; i++) scanf("%lld", &ramen[i]);
 	for(int i=0; i<n; i++){
 		if(ramen[i+1] > ramen[i+2]){
-			int k = min(ramen[i],ramen[i+1]-ramen[i+2]);
+			ll k = min(ramen[i],ramen[i+1]-ramen[i+2]);
 			ramen[i]-=k;
 			ramen[i+1]-=k;
 			cost+=(b+c)*k;
