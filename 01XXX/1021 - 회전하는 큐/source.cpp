dd22119a3fbd864831ccb17c05ce855726475adf
박성훈 / Koder <koder0205@gmail.com>
2021-04-22 19:48:34 +0900

    중간 전 마지막 커밋
    흑흑 중간고사 넘무싫어

--- 01XXX/1021 - 회전하는 큐/source.cpp
+++ 01XXX/1021 - 회전하는 큐/source.cpp
@@ -9,20 +9,20 @@ int n,m;
 int idx;
 int cnt;
 int ans=1234567890;
-int find;
+int findv;
 
 int main(){
 	scanf("%d %d", &n, &m);
 	for(int i=1; i<=n; i++) dq.push_back(i);
 	while(m--){
-		scanf("%d", &find);
-		for(int i=0; i<dq.size(); i++) if(dq[i] == find) idx = i;
+		scanf("%d", &findv);
+		for(int i=0; i<dq.size(); i++) if(dq[i] == findv) idx = i;
 		if(idx <= dq.size()-idx){
-			while(dq.front() != find){ cnt++; dq.push_back(dq.front()); dq.pop_front(); }
+			while(dq.front() != findv){ cnt++; dq.push_back(dq.front()); dq.pop_front(); }
 			dq.pop_front();
 		}
 		else{
-			while(dq.front() != find){ cnt++; dq.push_front(dq.back()); dq.pop_back(); }
+			while(dq.front() != findv){ cnt++; dq.push_front(dq.back()); dq.pop_back(); }
 			dq.pop_front();
 		}
 	}
---
+++ 20XXX/20928 - 걷는 건 귀찮아/source.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int p[123456] = {0};
+int x[123456] = {0};
+
+int dfs(int node, int k){
+	
+}
+
+int main(){
+	int n,m;
+	scanf("%d %d", &n, &m);
+	for(int i=0; i<n; i++) scanf("%d", &p[i]);
+	for(int i=0; i<n; i++) scanf("%d", &x[i]);
+	
+	
+}
