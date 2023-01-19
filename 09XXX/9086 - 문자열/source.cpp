0a420aad1f7b8059109612e8e322172f21f3cc4d
박성훈 / Koder <koder0205@gmail.com>
2023-01-19 15:57:41 +0900

    1솔튀
    브론즈 ㅠㅠ

---
+++ 09XXX/9086 - 문자열/source.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	for(int i=0; i<n; i++){
+		string s;
+		cin >> s;
+		cout << s[0] << s[s.length()-1] << "\n";
+	}
+}
